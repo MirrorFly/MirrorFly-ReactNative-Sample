c24e5c825a95fd378f245438aa0d3a5341dc4063
Karthikeyan S <karthikeyan.se@contus.in>
2023-04-28 18:21:04 +0530

    intial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import SplashScreen from './src/screen/SplashScreen';
+
+function App() {
+  return (
+    <SplashScreen />
+  );
+}
+
+export default App;
---
+++ src/screen/SplashScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Animated } from 'react-native';
+
+const SplashScreen = () => {
+    const logoOpacity = new Animated.Value(0);
+
+    React.useEffect(() => {
+        Animated.timing(logoOpacity, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Animated.Image
+                style={{ ...styles.logo, opacity: logoOpacity }}
+                source={require('../assets/mirrorfly-logo.png')}
+            />
+            {/* <Image source={require('../assets/mirrorfly-logo.png')} style={styles.logo} /> */}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+    },
+    logo: {
+        width: 300,
+        height: 350,
+        resizeMode: 'contain',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 20,
+    },
+});
+
+export default SplashScreen;

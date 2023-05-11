63d838dacbe0477f0f00e9493cb01b5e6b1c0b2c
Karthikeyan S <karthikeyan.se@contus.in>
2023-05-11 17:35:13 +0530

    register page solving

--- src/screen/RegisterScreen.js
+++ src/screen/RegisterScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Keyboard, StyleSheet, View, Image, Text, TextInput, TouchableOpacity, Linking, } from 'react-native';
+import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity, Linking, ToastAndroid, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { PrimaryPillBtn } from '../common/Button';
-import { Formik } from 'formik';
 import { useDispatch } from 'react-redux';
 import { navigate } from '../redux/navigationSlice';
 import { CONNECTED, COUNTRYSCREEN, PROFILESCREEN } from '../constant';
@@ -10,6 +9,9 @@ import { registerData } from '../redux/authSlice';
 
 const RegisterScreen = () => {
     const selectcountry = useSelector(state => state.navigation.selectContryCode);
+    const isLoading = useSelector(state=>state.auth.status);
+    const [mobileNumber, setMobileNumber] = React.useState('')
+  
 
     const isConnect = useSelector(state => state.auth.isConnected);
 
@@ -35,86 +37,92 @@ const RegisterScreen = () => {
         }
     }, [isConnect])
 
+    const handleSubmit = () => {
+
+        if(!mobileNumber){
+            return ToastAndroid.show('Please Enter Mobile Number', ToastAndroid.SHORT);
+        }
+        if (!/^[0-9]{10}$/i.test(mobileNumber)) {
+            return ToastAndroid.show('Please enter a valid mobile number', ToastAndroid.SHORT);
+        } else {
+            dispatch(registerData(selectcountry?.dial_code + mobileNumber))
+          
+        }
+
+    }
     return (
-       <Formik
-            initialValues={{ mobileNumber: '' }}
-            validate={values => {
-                const errors = {};
-                if (!values.mobileNumber) {
-                    errors.mobileNumber = 'Required';
-                }
-                //  else if (!/^[0-9]{10}$/i.test(values.mobileNumber)) {
-                // //     errors.mobileNumber = 'Invalid mobile number';
-                // // }
-                return errors;
-            }}
-            onSubmit={values => {
-                    dispatch(registerData(selectcountry?.dial_code + values.mobileNumber))
-            }}
-        >
-            {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
-                <View style={styles.headContainer}>
-                    <View style={styles.topSectionContainer}>
-                        <Image style={styles.imageView} resizeMode="cover" source={require('../assets/mobile.png')} />
-                        <Text style={styles.numberstyle}>Register Your Number</Text>
-                    </View>
-                    <View style={{ marginTop: 10 }}>
-                        <Text style={styles.chooseText} numberOfLines={2}>
-                            Please choose your country code and enter your mobile number to get the verification code.
+
+        <View style={styles.headContainer}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.topSectionContainer}>
+                <Image style={styles.imageView} resizeMode="contain" source={require('../assets/mobile.png')} />
+                <Text style={styles.numberstyle}>Register Your Number</Text>
+            </View>
+            <View style={{ marginTop: 10 }}>
+                <Text style={styles.chooseText} numberOfLines={2}>
+                    Please choose your country code and enter your mobile number to get the verification code.
+                </Text>
+            </View>
+            <View style={styles.flatListContainer} >
+                <TouchableOpacity
+                    onPress={selectCountryHandler}
+                    style={styles.dropstyle}
+                >
+                    <Text style={styles.countryText}>{selectcountry ? selectcountry?.name : 'India'}</Text>
+                    <Image resizeMode="contain" style={{height:12,width:12}}  source={require('../assets/DownArrow.png')} />
+                </TouchableOpacity>
+                <View style={styles.mainContainer}>
+                    <View style={styles.countryCodeContainer}>
+                        <Text style={styles.countryCodeText}>
+                            {selectcountry ? selectcountry?.dial_code : "+91"}
                         </Text>
+                        <View style={{ borderLeftWidth: 1, height: 20, borderColor: '#D3D3D3', marginLeft: 10, marginTop: 2 }} />
                     </View>
-                    <View style={styles.flatListContainer} >
-                        <TouchableOpacity
-                            onPress={selectCountryHandler}
-                            style={styles.dropstyle}
-                        >
-                            <Text style={styles.countryText}>{selectcountry?.name}</Text>
-                            <Image resizeMode="cover" source={require('../assets/DownArrow.png')} />
-                        </TouchableOpacity>
-                        <View style={styles.mainContainer}>
-                            <View style={styles.countryCodeContainer}>
-                                <Text style={styles.countryCodeText}>
-                                    {selectcountry?.dial_code}
-                                </Text>
-                                <View style={{ borderLeftWidth: 1, height: 20, borderColor: '#D5D5D5', marginLeft: 10, marginTop: 2 }} />
-                            </View>
-                            <TextInput
-                                style={styles.inputStyle}
-                                onChangeText={handleChange('mobileNumber')}
-                                onBlur={handleBlur('mobileNumber')}
-                                value={values.mobileNumber}
-                                placeholder='Enter mobile number'
-                                maxLength={10}
-                                placisLoadingholderTextColor={"#959595"}
-                                keyboardType="numeric"
-                                numberOfLines={1}
-                            />
-                        </View>
-                        {errors.mobileNumber && <Text style={{ color: 'red', marginHorizontal: 78 }}>{errors.mobileNumber}</Text>}
-                        <View style={styles.button}>
-                            <PrimaryPillBtn title='Continue' onPress={handleSubmit} />
-                        </View>
-                        <View style={styles.linkContainer}>
-                            <Text style={styles.titleText}>
-                                By clicking continue you agree to MirroFly
+                    {/* <KeyboardAvoidingView style={styles.TextInputContainer} >  */}
+                    <TextInput
+                        style={styles.inputStyle}
+                        onChangeText={(value) => {
+                            let num = value.replace(".", '');
+                            if (isNaN(num)) {
+                                ToastAndroid.show('Please Enter Number', ToastAndroid.SHORT);
+                            } else {
+                                setMobileNumber(value)
+                            }
+                        }}
+                        value={mobileNumber}
+                        placeholder='Enter mobile number'
+                        maxLength={15}
+                        placisLoadingholderTextColor={"#959595"}
+                        keyboardType="numeric"
+                        numberOfLines={1}
+                    />
+                    {/* </KeyboardAvoidingView> */}
+                </View>
+
+                <View style={styles.button}>
+              
+                    <PrimaryPillBtn title='Continue' isLoading={isLoading} onPress={() => { handleSubmit() }} />
+                </View>
+                <View style={styles.linkContainer}>
+                    <Text style={styles.titleText}>
+                        By clicking continue you agree to MirroFly
+                    </Text>
+                    <View style={styles.termserviceContainer}>
+                        <TouchableOpacity onPress={termsHandler} >
+                            <Text style={styles.termsText}>
+                                Terms and Conditions,
                             </Text>
-                            <View style={styles.termserviceContainer}>
-                                <TouchableOpacity onPress={termsHandler} >
-                                    <Text style={styles.termsText}>
-                                        Terms and Conditions,
-                                    </Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={PolicyHandler}>
-                                    <Text style={styles.policyText}>
-                                        Privacy Policy.
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={PolicyHandler}>
+                            <Text style={styles.policyText}>
+                                Privacy Policy.
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
-            )}
-        </Formik>
+            </View>
+            </ScrollView>
+        </View>
 
     )
 }
@@ -135,12 +143,12 @@ const styles = StyleSheet.create({
     },
     imageView: {
         marginBottom: 10,
-        width: 200,
-        height: 200
+        width:260,
+        height: 260
 
     },
     flatListContainer: {
-        marginTop: 42,
+        marginTop: 48,
         marginHorizontal: 4
 
     },
@@ -148,9 +156,12 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         borderBottomWidth: 1,
-        borderBottomColor: "#959595",
-        marginHorizontal: 6,
+        borderBottomColor: "#D3d3d3",
+        marginHorizontal: 12,
         paddingVertical: 12
+    },
+    TextInputContainer:{
+    
     },
     countryText:
     {
@@ -173,13 +184,13 @@ const styles = StyleSheet.create({
     },
     chooseText: {
         textAlign: "center",
-        marginHorizontal: 35,
+        marginHorizontal: 30,
         color: "#767676",
         fontSize: 14
     },
     button: {
         alignItems: 'center',
-        marginTop: 40,
+        marginTop: 42,
         fontWeight: "bold"
     },
     actionStyle: {
@@ -190,11 +201,9 @@ const styles = StyleSheet.create({
     },
     mainContainer: {
         flexDirection: "row",
-        justifyContent: "flex-start",
         alignItems: "center",
         marginHorizontal: 22,
         marginTop: 10,
-        position: "relative"
     },
     imageStyle: {
         width: 24,
@@ -207,7 +216,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
     linkContainer: {
-        marginTop: 24,
+        marginTop: 22,
         justifyContent: "center",
         alignItems: "center"
     },
@@ -249,13 +258,9 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
     inputStyle: {
-        marginTop: 2,
         fontSize: 14,
         fontWeight: "bold",
         color: "black",
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        paddingHorizontal: 18,
-        paddingVertical: 10
+        flex: 1
     }
 })

31f5fecb8636a2dfe1126b129a034ca32efa4120
Ashik Sikkanthar Abbas Manthir <ashiksikkanthar.a@contus.in>
2023-05-19 15:50:30 +0530

    Register  design issue fixed.

--- src/screen/RegisterScreen.js
+++ src/screen/RegisterScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Image, TextInput, TouchableOpacity, Linking, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { TouchableOpacity, Linking} from 'react-native';
 import { PrimaryPillBtn } from '../common/Button';
 import { useDispatch } from 'react-redux';
 import { navigate } from '../redux/navigationSlice';
-import { CONNECTED, COUNTRYSCREEN, numRegx, PROFILESCREEN } from '../constant';
+import { COUNTRYSCREEN, numRegx, PROFILESCREEN } from '../constant';
 import { useSelector } from 'react-redux';
 import { registerData } from '../redux/authSlice';
 import { getRecentChat } from '../redux/chatSlice';
 import { DownArrowIcon, RegiterPageIcon } from '../common/Icons';
-import { Icon, IconButton, Modal, Text, Center, Box, VStack, useToast, Spinner, HStack, Stack, Input } from 'native-base';
+import { Icon,  Modal, Text, Center, Box,useToast, Spinner, HStack, Stack, Input } from 'native-base';
 
 const RegisterScreen = () => {
     const dispatch = useDispatch();
@@ -16,7 +16,6 @@ const RegisterScreen = () => {
     const selectcountry = useSelector(state => state.navigation.selectContryCode);
     const isLoading = useSelector(state => state.auth.status);
     const [mobileNumber, setMobileNumber] = React.useState('')
-    const [loading, setloading] = React.useState(false);
 
     const termsHandler = () => {
         Linking.openURL("https://www.mirrorfly.com/terms-and-conditions.php");
@@ -42,7 +41,6 @@ const RegisterScreen = () => {
                 }
             })
         }
-
         if (mobileNumber.length <= '5') {
             return toast.show({
                 duration: 2500,
@@ -52,9 +50,7 @@ const RegisterScreen = () => {
                     </Box>;
                 }
             })
-
         }
-
         if (!/^[0-9]{10}$/i.test(mobileNumber)) {
             return toast.show({
                 duration: 2500,
@@ -64,7 +60,6 @@ const RegisterScreen = () => {
                     </Box>;
                 }
             })
-
         } else {
             dispatch(registerData(selectcountry?.dial_code + mobileNumber)).then((res) => {
                 dispatch(getRecentChat())
@@ -74,11 +69,9 @@ const RegisterScreen = () => {
         }
     }
     return (
-
         <>
             <Center h="full">
-                <Icon as={RegiterPageIcon} name="emoji-happy" height="100px" width="280px"
-                />
+                <Icon as={RegiterPageIcon} name="emoji-happy" height="100px" width="280px" />
                 <Text mt="15" fontWeight="600" fontSize="23"
                     color="black"
                 >Register Your Number</Text>
@@ -89,29 +82,22 @@ const RegisterScreen = () => {
                         Please choose your country code and enter your mobile {'\n'} number to get the verification code.
                     </Text>
                 </Center>
-
-
                 <TouchableOpacity onPress={selectCountryHandler}>
                     <HStack borderBottomWidth="1" py="2"
-
                         borderBottomColor="#f2f2f2" mt="50" space={80} justifyContent="flex-start" alignItems="center" >
                         <Text fontSize="15"
                             color="#181818" fontWeight="600" >{selectcountry?.name}</Text>
                         <Icon as={DownArrowIcon} name="emoji-happy" />
-
                     </HStack>
                 </TouchableOpacity>
-
                 <HStack flexDirection="row" alignItems="center" ml="6" mt="5"  >
-
                     <HStack mt="2" flexDirection="row" alignItems="center" mr="5"  >
-
                         <Text fontSize="16" color="black" fontWeight="600" mr="1">
                             +{selectcountry?.dial_code}
                         </Text>
                         <Stack height="8" ml="1" mt="2" borderLeftWidth="1" borderColor='#f2f2f2' />
                         <Input variant="unstyled"
-                            fontSize="14"
+                            fontSize="15"
                             fontWeight="500"
                             color="black"
                             flex="1"
@@ -128,22 +114,16 @@ const RegisterScreen = () => {
                             keyboardType="numeric"
                             numberOfLines={1}
                         />
-
                     </HStack>
                 </HStack>
-
                 <Stack alignItems='center' mt="42" fontWeight="bold">
-
                     <PrimaryPillBtn title='Continue' onPress={() => { handleSubmit() }} />
                 </Stack>
-
                 <Stack mt="22" justifyContent="center" alignItems="center" >
-
                     <Text color="#767676" fontSize="14" fontWeight="400">
                         By clicking continue you agree to MirroFly
                     </Text>
                     <HStack flexDirection="row" marginLeft="2" >
-
                         <TouchableOpacity onPress={termsHandler} >
                             <Text color="#3276E2" mr="1" fontSize="14" borderBottomWidth="1"
                                 borderBottomColor="#3276E2">

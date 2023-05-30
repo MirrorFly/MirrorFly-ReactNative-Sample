abfb7d33092320e056c14259e0cfc2219bf988f6
Ashik Sikkanthar Abbas Manthir <ashiksikkanthar.a@contus.in>
2023-05-30 19:45:51 +0530

    slice added completed .

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import navigationSlice from './navigationSlice';
 import authSlice from './authSlice';
 import chatSlice from './chatSlice';
+import profileSlice from './profileSlice';
 import userSlice from './userSlice';
 import storageSlice from './storageSlice';
 
@@ -10,8 +11,11 @@ const store = configureStore({
         navigation: navigationSlice,
         auth: authSlice,
         chat: chatSlice,
+        profile:profileSlice,
         user: userSlice,
         storage:storageSlice
+
+      
     },
 });
 

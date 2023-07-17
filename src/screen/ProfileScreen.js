a407c69c76a4cca03d5eb01412f30db45e38d9e6
Karthikeyan S <karthikeyan.se@contus.in>
2023-07-17 14:49:14 +0530

    #MRN-134 fixed

--- src/screen/ProfileScreen.js
+++ src/screen/ProfileScreen.js
@@ -32,7 +32,6 @@ const ProfileScreen = () => {
     if (Object.keys(selectProfileInfo).length === 0) {
       const userIdentifier = await AsyncStorage.getItem('userIdentifier')
       const profileDetails = await SDK.getUserProfile(JSON.parse(userIdentifier));
-      console.log('profileDetails', profileDetails)
       if (profileDetails.statusCode == 200) {
         AsyncStorage.setItem('vCardProfile', JSON.stringify(profileDetails.data))
         dispatch(profileDetail(profileDetails.data))

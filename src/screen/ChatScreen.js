64cdb8f24abde4ed3d28e8347df8e8a653e1d5b7
Karthikeyan S <karthikeyan.se@contus.in>
2023-07-31 16:08:53 +0530

    originalheight and originalwidth resolved

--- src/screen/ChatScreen.js
+++ src/screen/ChatScreen.js
@@ -129,7 +129,7 @@ function ChatScreen() {
         const file = files[i], msgId = uuidv4();
         const { caption = "", fileDetails = {}, fileDetails: { image: { fileSize, filename, playableDuration, uri }, type } = {} } = file;
         const msgType = type.split('/')[0];
-        const thumbImage = (msgType === "image" || msgType === "video") ? await getThumbImage(fileDetails.image) : ""
+        const thumbImage = (msgType === "image") ? await getThumbImage(fileDetails.image) : ""
         let fileOptions = {
           fileName: filename,
           fileSize: fileSize,

7d20e6c6d088706e74651c81e9bcf10d010f029c
Kadhar <kadharali.y@contus.in>
2023-08-01 11:00:38 +0530

    #MRN-158 sonar fix

--- src/components/Animate.js
+++ src/components/Animate.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { View } from 'native-base';
-import { Animated, Dimensions } from 'react-native';
+import {View} from 'native-base';
+import {Animated, Dimensions} from 'react-native';
 
-const Animate = ({ children }) => {
-    const scaleAnim = React.useRef(new Animated.Value(0)).current;
-    React.useEffect(() => {
-        Animated.timing(scaleAnim, {
-            toValue: 1,
-            duration: 100,
-            useNativeDriver: true,
-        }).start();
-    }, []);
-    const screenWidth = Dimensions.get('window').width;
-    const screenHeight = Dimensions.get('window').height;
-    const animatedStyle = {
-        transform: [{ scale: scaleAnim }],
-        width: screenWidth,
-        height: screenHeight,
-    };
+const Animate = ({children}) => {
+  const scaleAnim = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    Animated.timing(scaleAnim, {
+      toValue: 1,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
+  const animatedStyle = {
+    transform: [{scale: scaleAnim}],
+    width: screenWidth,
+    height: screenHeight,
+  };
 
-    return (
-        <View style={{ flex: 1 }}>
-            <Animated.View style={[animatedStyle, { flex: 1 }]}>
-                {children}
-            </Animated.View>
-        </View>
-    );
+  return (
+    <View style={{flex: 1}}>
+      <Animated.View style={[animatedStyle, {flex: 1}]}>
+        {children}
+      </Animated.View>
+    </View>
+  );
 };
 
 export default Animate;

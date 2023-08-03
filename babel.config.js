f08bae70dd22a4915679c27fbb1e542cc15a572c
Karthikeyan S <karthikeyan.se@contus.in>
2023-08-03 11:21:46 +0530

    ios config

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,5 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     // 'transform-remove-console',
-    [
-      'react-native-reanimated/plugin',
-      {
-        relativeSourceLocation: true,
-      },
-    ],
   ],
 };

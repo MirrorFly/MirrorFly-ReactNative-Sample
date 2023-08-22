e1e295113140a69192ecd49330ce5e9537b290e2
Abdul Rahman <abdulrahman.n@contus.in>
2023-08-22 12:32:48 +0530

    Added load more for forward contacts list

--- src/Helper/index.js
+++ src/Helper/index.js
@@ -112,3 +112,13 @@ export const getVideoThumbImage = async uri => {
   }
   return response;
 };
+
+export const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};

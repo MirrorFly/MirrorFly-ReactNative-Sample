1f20c9a48b915f70fd236439e480f236330c7b1e
Kadhar <kadharali.y@contus.in>
2023-09-13 09:15:01 +0530

    initial commit

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { ChatApp } from './src/ChatApp';
 
 function App() {
-  const API_URL = 'https://api-uikit-qa.contus.us/api/v1';
-  const QALisenceKey = 'ckIjaccWBoMNvxdbql8LJ2dmKqT5bp';
+  const API_URL = '';
+  const QALisenceKey = '';
   return (
     <ChatApp apiUrl={API_URL} licenseKey={QALisenceKey} isSandBox={false} />
   );

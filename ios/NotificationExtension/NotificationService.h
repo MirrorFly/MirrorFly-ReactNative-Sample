dde6f0d74a61b49fb64af74cfa063f73a6e7652c
Karthikeyan S <karthikeyan.se@contus.in>
2023-09-25 14:15:50 +0530

    initial commit for ios notification service

---
+++ ios/NotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationExtension
+//
+//  Created by user on 25/09/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

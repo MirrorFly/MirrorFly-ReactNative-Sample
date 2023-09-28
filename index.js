054e08692f6d31d865956bb36810a8d7c9040594
Karthikeyan S <karthikeyan.se@contus.in>
2023-09-28 19:07:57 +0530

    notification decrypted in IOS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import { AppRegistry, Platform } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import 'react-native-get-random-values';
 import messaging from '@react-native-firebase/messaging';
 import SDK from './src/SDK/SDK';
-import { pushNotify } from './src/Service/remoteNotifyHandle';
+import {
+  pushNotify,
+  updateNotification,
+} from './src/Service/remoteNotifyHandle';
 import {
   getNotifyMessage,
   getNotifyNickName,
@@ -20,17 +23,23 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
       'setBackgroundMessageHandler remoteMessage',
       JSON.stringify(remoteMessage, null, 2),
     );
+    if (remoteMessage.data.type === 'recall') {
+      updateNotification(remoteMessage.data.message_id);
+      return;
+    }
     const notify = await SDK.getNotificationData(remoteMessage);
     console.log(notify, 'notify?.message');
     if (notify?.statusCode === 200) {
       updateRecentAndConversationStore(notify?.data);
-      pushNotify(
-        notify?.data?.msgId,
-        getNotifyNickName(notify?.data),
-        getNotifyMessage(notify?.data),
-        notify?.data?.fromUserJid,
-        false,
-      );
+      if (notify?.data?.type === 'receiveMessage') {
+        pushNotify(
+          notify?.data?.msgId,
+          getNotifyNickName(notify?.data),
+          getNotifyMessage(notify?.data),
+          notify?.data?.fromUserJid,
+          false,
+        );
+      }
       handleSetPendingSeenStatus(notify?.data);
     }
   } catch (error) {
--- ios/NotificationExtension/NotificationService.m
+++ ios/NotificationExtension/NotificationService.m
@@ -6,6 +6,7 @@
 //
 
 #import "NotificationService.h"
+#import "NotificationExtension-Swift.h"
 
 @interface NotificationService ()
 
@@ -21,11 +22,39 @@
     self.bestAttemptContent = [request.content mutableCopy];
    NSString *messagecontent = [self.bestAttemptContent.userInfo objectForKey:@"message_content"];
    NSString *userjid = [self.bestAttemptContent.userInfo objectForKey:@"from_user"];
-
+   NSString *message_id = [self.bestAttemptContent.userInfo objectForKey:@"message_id"];
    NSLog(@"[NotificationService messagecontent]%@ ", messagecontent );
    NSLog(@"[NotificationService userjid]%@ ", userjid);
+  @autoreleasepool {
+    MyClass * obj = [MyClass new];
+    NSString *res = [obj runWithMessageID: message_id content:messagecontent];
+    NSString *jsonString = res;
+
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+
+    id jsonObject = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+
+    if ([jsonObject isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *jsonDict = (NSDictionary *)jsonObject;
+        
+        NSString *message = jsonDict[@"message"];
+        NSString *messageType = jsonDict[@"message_type"];
+        NSArray *mentionedUserIds = jsonDict[@"mentionedUsersIds"];
+        NSString *nickName = jsonDict[@"nickName"];
+        
+        NSLog(@"Message: %@", message);
+        NSLog(@"Message Type: %@", messageType);
+        NSLog(@"Mentioned User IDs: %@", mentionedUserIds);
+        NSLog(@"Nick Name: %@", nickName);
+    } else {
+        NSLog(@"Error parsing JSON: %@", error);
+    }
+    NSLog(@"[NotificationService res]%@", res);
+    self.bestAttemptContent.title = res;
+  }
     // Modify the notification content here...
-    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
+//    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
     
     self.contentHandler(self.bestAttemptContent);
 }

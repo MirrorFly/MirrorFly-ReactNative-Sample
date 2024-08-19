4c0295f9656d641e88b65770c034ee9920e7780f
Karthikeyan S <karthikeyan.se@contus.in>
2024-08-19 18:30:32 +0530

    MRN-756,MRN-758, MRN-744, MRN-729, MRN-699, MRN-690

--- src/uikitMethods.js
+++ src/uikitMethods.js
@@ -13,8 +13,10 @@ import { setupCallKit } from './calls/ios';
 import { setNotificationForegroundService } from './calls/notification/callNotifyHandler';
 import { getNotifyMessage, getNotifyNickName, getUserIdFromJid, showToast } from './helpers/chatHelpers';
 import { MIX_BARE_JID } from './helpers/constants';
+import { addChatMessageItem } from './redux/chatMessageDataSlice';
 import { clearState } from './redux/clearSlice';
-import { getRoasterData } from './redux/reduxHook';
+import { addRecentChatItem } from './redux/recentChatDataSlice';
+import { getArchive, getRoasterData } from './redux/reduxHook';
 import { setRoasterData } from './redux/rosterDataSlice';
 import store from './redux/store';
 import { RECENTCHATSCREEN, REGISTERSCREEN } from './screens/constants';
@@ -75,6 +77,9 @@ export const mirrorflyNotificationHandler = async remoteMessage => {
                getNotifyMessage(notify?.data),
                notify?.data?.fromUserJid,
             );
+            notify.data.archiveSetting = getArchive();
+            store.dispatch(addRecentChatItem(notify?.data));
+            store.dispatch(addChatMessageItem(notify?.data));
          }
       }
    } catch (error) {
@@ -148,7 +153,7 @@ export const mirrorflyRegister = async ({ userIdentifier, fcmToken = '', metadat
                return connect;
          }
       } else {
-         showToast(registerRes.message, Toast.SHORT);
+         showToast(registerRes.message);
          return registerRes;
       }
    } catch (error) {

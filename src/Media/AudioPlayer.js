a15aa4a29acd34d7eb652ba708da5741a8547620
Karthikeyan S <karthikeyan.se@contus.in>
2025-02-07 12:34:04 +0530

    MRN-850

--- src/Media/AudioPlayer.js
+++ src/Media/AudioPlayer.js
@@ -145,9 +145,11 @@ const AudioPlayer = props => {
    };
 
    const getAudioTimeString = seconds => {
-      let m = parseInt(seconds / 60, 10);
-      let s = parseInt(seconds % 60, 10);
-      return (m < 10 ? `0${m}` : m) + ':' + (s < 10 ? `0${s}` : s);
+      let h = Math.floor(seconds / 3600);
+      let m = Math.floor((seconds % 3600) / 60);
+      let s = Math.floor(seconds % 60);
+
+      return (h > 0 ? (h < 10 ? `0${h}` : h) + ':' : '') + (m < 10 ? `0${m}` : m) + ':' + (s < 10 ? `0${s}` : s);
    };
 
    const playComplete = success => {
